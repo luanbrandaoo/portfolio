9954e5079f775bfbffcb207e05e1311c59710927
Luan Brandão <97369040+luanbrandaoo@users.noreply.github.com>
2024-05-16 19:00:35 +0000

    taskbar icons styles

--- src/components/programTaskbar.tsx
+++ src/components/programTaskbar.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
-import './components.css'
+import React, { useState } from 'react';
+import './components.css';
 
 const ProgramTaskbar = (props) => {
+    const [click, setClick] = useState(false);
+    const [active, setActive] = useState(false);
+
+    const handleMouseDown = () => {
+        setClick(true);
+    };
+
+    const handleMouseUp = () => {
+        setClick(false);
+        if (!active) {
+            setActive(true);
+        }
+    };
+
     return (
-        <div className="h-8 programTaskbar flex align-center justify-center px-2">
-            <img src={props.icon} className="h-6 mt-1"></img>
-            <span className="text-black font-ms font-normal text-start mt-1 ml-1">{props.programName}</span>
+        <div
+            className={`h-8 programTaskbar flex align-center justify-center px-2 ${
+                click ? 'programTaskbarClick' : ''
+            } ${active ? 'programTaskbarActive' : ''}`}
+            onMouseDown={handleMouseDown}
+            onMouseUp={handleMouseUp}
+        >
+            <img src={props.icon} className="h-6 mt-1" alt="Program Icon"></img>
+            <span className="text-black font-ms font-normal text-start mt-1 ml-1">
+                {props.programName}
+            </span>
         </div>
-      );
-    }
+    );
+};
 
 export default ProgramTaskbar;
