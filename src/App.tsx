b9c33d68e872422c29e655448f4bca92db6a5e96
Luan Brandão <97369040+luanbrandaoo@users.noreply.github.com>
2024-05-16 19:16:27 +0000

    desktop created

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
+import Desktop from './components/desktop/desktop'
 import Taskbar from './components/taskbar/taskbar'
+
 
 export default function App() {
   return (
     <div className="h-screen flex flex-col">
-      <main className="flex-grow">
-        Main content
-      </main>
+      <Desktop />
       <Taskbar />
     </div>
   );
---
+++ src/components/desktop/desktop.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Desktop = () => {
+  return (
+    <main className="bg-desktop h-full">
+        <h1>Main Content</h1>
+    </main>
+  );
+}
+
+export default Desktop;
