0bccf3d6c9000b1c96fa023ee48927511de1ebd0
Luan Brandão <97369040+luanbrandaoo@users.noreply.github.com>
2024-05-21 21:20:51 +0000

    dragging windows

--- src/components/window/window.tsx
+++ src/components/window/window.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
+import Draggable from 'react-draggable';
+
 import './window.css';
 
 import useProgramStore from '../programStore';
 
 const Janela = ({programName, icon, children}) => {
-  const [isDragging, setIsDragging] = useState(false);
   const { programs, setPosition } = useProgramStore(state => ({
     programs: state.programs,
     setPosition: state.setPosition
@@ -13,21 +14,30 @@ const Janela = ({programName, icon, children}) => {
   const program = programs.find(p => p.programName === programName);
 
 
+  const [initialPosition, setInitialPosition] = useState({ x: 0, y: 0 });
+  useEffect(() => {
+      setInitialPosition(program.position);
+  }, []);
+
+
+  const dragRef = useRef(null);
+  const [dragging, setDragging] = useState(false);
+
+  const handleStartDragging = () => {
+    setDragging(true);
+  };
+
+  const handleStopDragging = () => {
+    const drag = dragRef.current.getBoundingClientRect();
+    console.log(drag.left, drag.top);
+    setPosition(programName, drag.left, drag.top);
+    setDragging(false);
+  };
+
+
   const [clickMinimize, setClickMinimize] = useState(false);
   const [clickMaximize, setClickMaximize] = useState(false);
   const [clickClose, setClickClose] = useState(false);
-
-
-  const handleDragging  = () => {
-    console.log('dragging')
-    setIsDragging(true);
-    setPosition(programName, 2, 2);
-  };
-
-  const handleStopDragging  = () => {
-    setIsDragging(false);
-  };
-
 
   const handleMouseDownMinimize = () => {
     setClickMinimize(true);
@@ -54,28 +64,35 @@ const Janela = ({programName, icon, children}) => {
   };
 
   return (
-    <div className={"h-[32rem] w-[42rem] bg-silver window z-10 flex flex-col"}
-        style={{ position: 'absolute', top: `${program.position.y}px`, left: `${program.position.x}px` }}
-        onMouseDown={handleDragging} onMouseUp={handleStopDragging}>
-      <div className="h-9 w-auto titleBox bg-windowblue flex flex-row justify-between">
-        <div className='flex flex-row gap-2 align-center'>
-          <img src={icon} className="h-6"></img>
-          <span className='text-white font-ms font-normal text-start'>{programName}</span>
+    <div>
+      <Draggable handle=".handle" onStart={handleStartDragging} onStop={handleStopDragging}>
+        <div ref={dragRef} className={`h-[32rem] w-[42rem] pointernone z-20 ${dragging ? 'dragWindow' : ''}`}
+            style={{ position: 'absolute', top: `${initialPosition.y}px`, left: `${initialPosition.x}px` }}>
+          <div className="h-9 w-auto titleBox pointerauto handle"></div>
         </div>
-        <div className='flex flex-row gap-1 align-center m-px'>
-          <div className={`bg-silver h-6 w-6 windowButtons flex align-center justify-center  ${clickMinimize ? 'windowButtonsClick' : ''}`} onMouseDown={handleMouseDownMinimize} onMouseUp={handleMouseUpMinimize}>
-            <span className='text-black font-ms font-black text-window -m-1'>_</span>
+      </Draggable>
+      <div className={"h-[32rem] w-[42rem] bg-silver window z-10 flex flex-col"}
+          style={{ position: 'absolute', top: `${program.position.y}px`, left: `${program.position.x}px` }}>
+        <div className="h-9 w-auto titleBox bg-windowblue flex flex-row justify-between">
+          <div className='flex flex-row gap-2 align-center'>
+            <img src={icon} className="h-6"></img>
+            <span className='text-white font-ms font-normal text-start'>{programName}</span>
           </div>
-          <div className={`bg-silver h-6 w-6 windowButtons flex align-center justify-center  ${clickMaximize ? 'windowButtonsClick' : ''}`} onMouseDown={handleMouseDownMaximize} onMouseUp={handleMouseUpMaximize}>
-            <div className='border-black border-2 border-t-4 h-3 w-3 m-1'></div>
-          </div>
-          <div className={`bg-silver h-6 w-6 windowButtons flex align-center justify-center  ${clickClose ? 'windowButtonsClick' : ''}`} onMouseDown={handleMouseDownClose} onMouseUp={handleMouseUpClose}>
-            <span className='text-black font-ms font-black text-window -m-px'>X</span>
+          <div className='flex flex-row gap-1 align-center m-px'>
+            <div className={`bg-silver h-6 w-6 windowButtons flex align-center justify-center  ${clickMinimize ? 'windowButtonsClick' : ''}`} onMouseDown={handleMouseDownMinimize} onMouseUp={handleMouseUpMinimize}>
+              <span className='text-black font-ms font-black text-window -m-1'>_</span>
+            </div>
+            <div className={`bg-silver h-6 w-6 windowButtons flex align-center justify-center  ${clickMaximize ? 'windowButtonsClick' : ''}`} onMouseDown={handleMouseDownMaximize} onMouseUp={handleMouseUpMaximize}>
+              <div className='border-black border-2 border-t-4 h-3 w-3 m-1'></div>
+            </div>
+            <div className={`bg-silver h-6 w-6 windowButtons flex align-center justify-center  ${clickClose ? 'windowButtonsClick' : ''}`} onMouseDown={handleMouseDownClose} onMouseUp={handleMouseUpClose}>
+              <span className='text-black font-ms font-black text-window -m-px'>X</span>
+            </div>
           </div>
         </div>
-      </div>
-      <div className='bg-border2 contentbox h-full'>
-        {children}
+        <div className='bg-border2 contentbox h-full'>
+          {children}
+        </div>
       </div>
     </div>
   );
