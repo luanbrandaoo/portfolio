e9ec501811a8f1f693a436e5798684380d6c92ec
Luan Brandão <97369040+luanbrandaoo@users.noreply.github.com>
2024-11-13 18:10:03 +0000

    open programs from start menu

--- src/components/taskbar/start/startMenuComponents.tsx
+++ src/components/taskbar/start/startMenuComponents.tsx
@@ -1,9 +1,18 @@
 import './startMenu.css';
+import useProgramStore from '../../programStore';
+import useStartMenuStore from './startmenuStore';
 
 const ProgramStart = ({programName, logo}) => {
+    const addProgram = useProgramStore((state) => state.addProgram);
+    const { setStartOpen } = useStartMenuStore();
+    const handleMouseClick = () => {
+        addProgram({programName, logo});
+        setStartOpen(false);
+
+    };
 
     return (
-        <div className='h-[54px] w-full flex pl-4 items-center text-black hover:bg-windowblue hover:text-white'>
+        <div className='h-[54px] w-full flex pl-4 items-center text-black hover:bg-windowblue hover:text-white' onClick={handleMouseClick}>
             <img src={logo} className={'h-10'} draggable="false"></img>
             <span className='ml-2 pb-1 font-ms font-light text-start'>{programName}</span>
         </div>
