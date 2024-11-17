19728d42c074b869f75b8e7a67f0e1e44a91b3c8
Luan Brandão <97369040+luanbrandaoo@users.noreply.github.com>
2024-11-17 22:26:30 +0000

    default context menu disabled

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import { useState } from 'react'
-import Desktop from './components/desktop/desktop'
-import Taskbar from './components/taskbar/taskbar'
-
+import { useEffect } from 'react';
+import Desktop from './components/desktop/desktop';
+import Taskbar from './components/taskbar/taskbar';
 
 export default function App() {
+  useEffect(() => {
+    const handleContextMenu = (event) => {
+      event.preventDefault();
+    };
+    
+    document.addEventListener('contextmenu', handleContextMenu);
+
+    return () => {
+      document.removeEventListener('contextmenu', handleContextMenu);
+    };
+  }, []);
+
   return (
     <div className="h-screen flex flex-col select-none">
       <Desktop />
